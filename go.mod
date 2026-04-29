49f371b465de98106d26ff7e1b72063639546c13
Максим Кожекин <palach608@gmail.com>
2026-04-29 20:38:07 +0300

    Add infrastructure Layer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/palach608/go-platform
 
 go 1.25.6
+
+require golang.org/x/crypto v0.50.0
--- services/auth/internal/application/service.go
+++ services/auth/internal/application/service.go
@@ -1,1 +1,28 @@
 package application
+
+import (
+	"context"
+
+	"github.com/palach608/go-platform/services/auth/internal/domain"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService struct {
+	repo domain.UserRepository
+}
+
+func NewAuthService(repo domain.UserRepository) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) Register(ctx context.Context, username, email, password string) error {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	user := &domain.User{
+		Username: username,
+		Email:    email,
+		Password: string(hashedPassword),
+	}
+
+	return s.repo.Create(ctx, user)
+}
