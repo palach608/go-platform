76f0c38beaba300a69fcff09d5b6c92f351a43f5
Максим Кожекин <palach608@gmail.com>
2026-04-29 20:35:09 +0300

    Add user model

--- services/auth/internal/domain/user.go
+++ services/auth/internal/domain/user.go
@@ -1,1 +1,15 @@
 package domain
+
+import "context"
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+}
+
+type UserRepository interface {
+	Create(ctx context.Context, user *User) error
+	GetByEmail(ctx context.Context, email string) (*User, error)
+}
